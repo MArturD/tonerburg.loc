090dcb30ac8df9a62593d3ee1c0776eaeae753f7
Ivan <it-1ivan@ya.ru>
2023-09-14 21:57:19 +0500

    start moduls banner

--- wp-content/themes/tonerburg/index.php
+++ wp-content/themes/tonerburg/index.php
@@ -5,59 +5,8 @@
  */
 ?>
 
+<?php include get_template_directory() . "/moduls/banner.php"; ?>
 
-<div class="banner margin-bottom">
-    <div class="wrap">
-        <div class="banner-wrap">
-            <div class="banner-wrap-left">
-                <div class="banner-wrap-left__title "><?php echo get_field("test", "options") ?>Продайте новые и оригинальные картриджи быстро и по выгодной цене
-                </div>
-                <div class="banner-wrap-left__information">По фото
-                    в Whatsapp или Telegram или позвонив нам на телефон</div>
-                <div class="banner-wrap-left__description">
-                    <div class="banner-wrap-left__img">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/images/icons/printer-icon.svg">
-                    </div>
-                    <div class="banner-wrap-left__description-title text-font">
-                        Пришлите фото или название в чат и узнайте стоимость выкупа в <span
-                            class="banner-wrap-left_fat">течение 10 минут</span>
-                    </div>
-                </div>
-                <div class="banner-contacts">
-                    <div class="contacts-icons">
-                        <div class="contacts-icons__item">
-                            <a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/images/icons/mail.svg"></a>
-                        </div>
-                        <div class="contacts-icons__item">
-                            <a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/images/icons/tg.svg"></a>
-                        </div>
-                        <div class="contacts-icons__item">
-                            <a href="#"><img src="<?php bloginfo('template_url'); ?>/assets/images/icons/whats.svg"></a>
-                        </div>
-                    </div>
-
-                    <div class="banner-contacts__phone">
-                        <a href="#" class="banner-contacts__buttom btn-hover button-font">Позвонить нам</a>
-                        <div class="banner-contacts__number">
-                            <div class="contacts__img">
-                                <img src="<?php bloginfo('template_url'); ?>/assets/images/icons/phone.svg">
-                            </div>
-                            <a href="tel:">+7(953)363-50-50</a>
-                        </div>
-                        </a>
-                    </div>
-
-                </div>
-
-
-            </div>
-            <div class="banner-wrap-right">
-                <img src="<?php bloginfo('template_url'); ?>/assets/images/others/banner.svg">
-            </div>
-
-        </div>
-    </div>
-</div>
 <div class="advantages margin-bottom">
     <div class="wrap">
         <div class="advantages-wrap">
