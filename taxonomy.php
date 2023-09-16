088332ffa817983507e4808c5f86e1fa5113953f
Ivan <it-1ivan@ya.ru>
2023-09-16 17:26:47 +0500

    получили термины

--- taxonomy.php
+++ taxonomy.php
@@ -1,6 +1,45 @@
 <?php get_header(); ?>
-<h1>Это наши бренду будут</h1>
+	<h1>Это наши бренду будут</h1>
 
-	<img src="<?php echo get_image("logo");?>" alt="">
+	<img src="<?php echo get_image("logo"); ?>" alt="">
+
+<?php
+
+$queried_object = get_queried_object();
+$taxonomy       = $queried_object->taxonomy;
+$term_id        = $queried_object->term_id;
+
+// задаем нужные нам критерии выборки данных из БД
+$args = array(
+	'post_type' => 'cartridge',
+	'tax_query' => [
+		'relation' => 'AND',
+		[
+			'taxonomy' => 'brand',
+			'field'    => 'id',
+			'terms'    => array($term_id),
+		]
+	]
+);
+
+$query = new WP_Query($args);
+
+// Цикл
+if ($query->have_posts()) {
+	while ($query->have_posts()) {
+		$query->the_post();
+		?>
+		<li><?php the_title() ?></li>
+		<?php
+	}
+} else {
+	// Постов не найдено
+}
+
+// Возвращаем оригинальные данные поста. Сбрасываем $post.
+wp_reset_postdata();
+
+
+?>
 
 <?php get_footer(); ?>
