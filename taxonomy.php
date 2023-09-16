e49ddcb90df7d1b56c9b57445a63ca8d701e894b
Ivan <it-1ivan@ya.ru>
2023-09-16 16:42:55 +0500

    template taxonomy

--- taxonomy.php
+++ taxonomy.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 <h1>Это наши бренду будут</h1>
 
+<?php var_dump(get_field("logo"));
+$image = get_field("logo");
+if( $image ) {
+	$logo = wp_get_attachment_image_url( $image, 'full' );
+} else{
+	$logo = get_bloginfo('template_url') . "/assets/images/others/location-img.png";
+}
+
+?>
+
+	<img src="<?php echo $logo;?>" alt="">
 
 <?php get_footer(); ?>
