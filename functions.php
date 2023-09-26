9b1253d29e9e8b9e0b7c22984ab35b9767c56adf
Artur <2004misiryun@gmail.com>
2023-09-26 07:11:30 +0300

    burger - fixed 100% form search 80%

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@ function ii_esp_search_ajax_action_callback() {
 				$cur_terms = get_the_terms( get_the_ID(), 'brand' );
 				if( is_array( $cur_terms ) ){
 
-						echo '<a href="'. get_term_link( $cur_terms[0]->term_id, $cur_terms[0]->taxonomy ) .'" class="search-item"><p>'. get_the_title() .  '</p> <p>'. get_field('article') .'</p> </a>';
+						echo '<a href="'. get_term_link( $cur_terms[0]->term_id, $cur_terms[0]->taxonomy ) .'" class="search-item"><p>'. get_the_title() .  '</p> <p>'. get_field('price') .'</p> </a>';
 
 				}
                 ?>
--- header.php
+++ header.php
@@ -48,7 +48,7 @@
                 ]);
                 ?>
             </nav>
-            <form action="<?php esc_url( home_url( '/' ) ); ?>" class="form-search " method="post">
+            <form action="<?php esc_url( home_url( '/' ) ); ?>" class="form-search" method="post">
 <!--                --><?php // get_search_form() ?>
                 <input name="s" value="<?php get_search_query(); ?>" type="text" class="form-search__input" placeholder="Поиск" autocomplete="off">
                 <div class="form-search__img">
