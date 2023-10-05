fb7f81cafeed825255d7e094e0aa8eb6b12ca09d
Ivan <it-1ivan@ya.ru>
2023-10-05 22:09:31 +0500

    fix search

--- header.php
+++ header.php
@@ -48,9 +48,9 @@
                 ]);
                 ?>
             </nav>
-            <form action="<?php esc_url( home_url( '/' ) ); ?>" class="form-search" method="post">
+            <form action="#" class="form-search" method="post">
 <!--                --><?php // get_search_form() ?>
-                <input name="s" value="<?php get_search_query(); ?>" type="text" class="form-search__input" placeholder="Поиск" autocomplete="off">
+                <input name="s" value="<?php echo $_REQUEST['s']; ?>" type="text" class="form-search__input" placeholder="Поиск" autocomplete="off">
                 <div class="form-search__img">
                     <img alt="" src="<?php bloginfo('template_url'); ?>/assets/images/icons/search.svg">
                 </div>
