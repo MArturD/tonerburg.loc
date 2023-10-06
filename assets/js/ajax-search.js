12a3bb9a79e956c085fe1b6cdd3cf052d3ceedbc
Ivan <it-1ivan@ya.ru>
2023-10-06 11:10:12 +0500

    DONE SEARCH

--- assets/js/ajax-search.js
+++ assets/js/ajax-search.js
@@ -1,14 +1,22 @@
-jQuery(function ($) {
+jQuery(document).ready(function ($) { //ожидание полной загрузки дом дерева и возвожность работы со знаком доллара, в движках
+
 	$('.form-search input[name="s"]').on('click keyup', function () {
+		var input = $(this);
 		var search = $('.form-search input[name="s"]').val();
-		if (search.length < 2) {
+
+		var result = $(".form-search__result");
+
+		if (!search.length) {
+			result.html('');
 			return false;
 		}
+
 		var data = {
 			s: search,
 			action: 'search-ajax',
 			nonce: searchForm.nonce
 		};
+
 		$.ajax({
 			url: searchForm.url,
 			// url: 'http://tonerburg/wp-admin/admin-ajax.php',
@@ -17,26 +25,22 @@ jQuery(function ($) {
 			dataType: 'json',
 
 			beforeSend: function (xhr) {
-				$(".form-search__result").html(
+				result.html(
 					'<div class="ii-search-result__no-results">\n' +
 					'            <p>Загрузка...</p>\n' +
 					'        </div>'
 				);
 			},
 			success: function (data) {
-				// console.log('asdfa');
-				$(".form-search__result").html(data.out);
+				result.addClass('search-block').html(data.out);
+				myFocus(input);
 			}
 		});
 	});
-});
 
-jQuery(function ($) {
 	$('.search-catalog input[name="s"]').on('keyup', function () {
+		var input = $(this);
 		var search = $('.search-catalog input[name="s"]').val();
-		// if (search.length < 2) {
-		// 	return false;
-		// }
 		var data = {
 			s: search,
 			term_id: term_id,
@@ -53,15 +57,12 @@ jQuery(function ($) {
 			beforeSend: function (xhr) {
 				$(".catalog-table__one tbody").html(`<tr>
                                     <td colspan="2" class="catalog-table__line_one">Загрузка...</td>
-                                  
                                 </tr>`);
 			},
 			success: function (data) {
 				$(".catalog-table__one tbody").html(data.out);
+				myFocus(input);
 			}
 		});
 	});
 });
-
-
-
--- assets/js/main.js
+++ assets/js/main.js
@@ -15,103 +15,135 @@
 //     });
 // });
 // retrieveData.js
-$(document).ready(function () {
-    // Извлекаем значение из localStorage
-    // var searchInputValue = localStorage.getItem('searchInputValue');
 
-    // Устанавливаем значение в .catalog-search__input
-    // $('.catalog-search__input').val(searchInputValue);
-});
-$(document).ready(function () {
-    $('.form-search__img').on('click', function () {
-        $('.form-search__input').focus().select();
-    });
-});
+function myFocus($input) {
+	var $input = jQuery($input);
 
-function searchClick() {
-    $('.form-search').click(function() {
-        $('.menu').addClass('menu-block');
-        $('.form-search__result').addClass('search-block');
-    });
+	// Сфокусируемся на поле ввода
+	$input.focus();
+
+	// Переместим курсор в конец текста
+	var inputLength = $input.val().length;
+	$input[0].setSelectionRange(inputLength, inputLength);
 }
-searchClick();
 
-$('.burger-btn').on('click', function () {
+jQuery(document).ready(function ($) { //ожидание полной загрузки дом дерева и возвожность работы со знаком доллара, в движках
 
-    $('.form-search').addClass('dn_mobile').removeAttr('style');
+	// Извлекаем значение из localStorage
+	// var searchInputValue = localStorage.getItem('searchInputValue');
 
-    var burder = $('.burger-menu');
-    if (burder.is(':visible')){
-        burder.slideUp(300);
-    } else{
-        burder.slideDown(300);
-    }
-    // $('.burger-menu').slideToggle(function (){
-    //     console.log('222');
-    //
-    //     if ($(this).is(':visible'))
-    //         $(this).css('display', 'flex');
-    // });
-    // $('.form-search').slideToggle(function (){
-    //     if ($(this).is(':visible'))
-    //         $(this).css('display', 'none');
-    // });
-    // $('.burger-wrap').toggleClass('burger-wrap-active')
+	// Устанавливаем значение в .catalog-search__input
+	// $('.catalog-search__input').val(searchInputValue);
 
-})
-function search_close() {
-    $('.form-search__img').click(function() {
-        $('.burger-wrap').removeClass('burger-wrap-active');
-        $('.form-search').removeClass('form-search-none');
-        $('.burger-menu').css('display', 'none');
-    });
-}
-search_close();
+// $(document).ready(function () {
+	// $('.form-search__img').on('click', function () {
+	//     $('.form-search__input').focus().select();
+	// });
+// });
 
-$('.search__img').on('click', function () {
+	function searchClick() {
+		$('.form-search').click(function () {
+			$('.menu').addClass('menu-block');
+			$('.form-search__result').addClass('search-block');
+		});
+	}
 
-    $('.form-search').slideToggle(function (){
-        if ($(this).is(':visible'))
-            $(this).css('display', 'flex');
-    });
-    $('.burger-menu').removeClass(function (){
-        if ($(this).is(':visible'))
-            $(this).css('display', 'none');
-    });
-    $('.burger-wrap').removeClass('burger-wrap-active')
+	searchClick();
 
-})
 
-function searchClose(){
-$(document).click(function (e) {
-    if ($(e.target).closest(".form-search").length) {
-        // клик внутри элемента
-        return;
-    }
-    // клик снаружи элемента
-    $(".menu").removeClass('menu-block');
-    $('.form-search__result').removeClass('search-block');
+	$(".form-search__input").focus(function () { // задаем функцию при получении фокуса элементом <form-search__input>
+		$('.form-search').trigger('click');
+	});
+	$(".form-search__input").blur(function () { // задаем функцию при потери фокуса элементом <form-search__input>
 
-});
-}
-searchClose();
+	});
 
-$('.autoplay').slick({
-    arrows: true,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next'),
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    // centerMode: true,
-    responsive: [
-        {
-            breakpoint: 390,
-            settings: {
-                centerMode: true,
-            }
-        }
-        ]
+	$('.burger-btn').on('click', function () {
+
+		$('.form-search').addClass('dn_mobile').removeAttr('style');
+
+		var burder = $('.burger-menu');
+		if (burder.is(':visible')) {
+			burder.slideUp(300);
+		} else {
+			burder.slideDown(300);
+		}
+		// $('.burger-menu').slideToggle(function (){
+		//     console.log('222');
+		//
+		//     if ($(this).is(':visible'))
+		//         $(this).css('display', 'flex');
+		// });
+		// $('.form-search').slideToggle(function (){
+		//     if ($(this).is(':visible'))
+		//         $(this).css('display', 'none');
+		// });
+		// $('.burger-wrap').toggleClass('burger-wrap-active')
+
+	})
+
+	function search_close() {
+		$('.form-search__img').click(function () {
+			$('.burger-wrap').removeClass('burger-wrap-active');
+			$('.form-search').removeClass('form-search-none');
+			$('.burger-menu').css('display', 'none');
+		});
+	}
+
+	search_close();
+
+	$('.search__img').on('click', function () {
+
+		var input = $('.form-search__input');
+		if (input.val().length) {
+			input.trigger('click');
+		}
+
+		$('.form-search').slideToggle(function () {
+			if ($(this).is(':visible'))
+				$(this).css('display', 'flex');
+		})
+
+		$('.burger-menu').removeClass(function () {
+			if ($(this).is(':visible'))
+				$(this).css('display', 'none');
+		});
+		$('.burger-wrap').removeClass('burger-wrap-active')
+
+	})
+
+	function searchClose() {
+		$(document).click(function (e) {
+			if ($(e.target).closest(".form-search").length) {
+				// клик внутри элемента
+				return;
+			}
+			// клик снаружи элемента
+			$(".menu").removeClass('menu-block');
+			$('.form-search__result').removeClass('search-block');
+
+		});
+	}
+
+	searchClose();
+
+	$('.autoplay').slick({
+		arrows: true,
+		prevArrow: $('.prev'),
+		nextArrow: $('.next'),
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		variableWidth: true,
+		// centerMode: true,
+		responsive: [
+			{
+				breakpoint: 390,
+				settings: {
+					centerMode: true,
+				}
+			}
+		]
+	});
 });
 
